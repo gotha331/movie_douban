6e4730aabaa9bfa181fb85a7d51e59562ad68a64
rivers-guitar <wyn@520.com>
2017-12-11 16:13:12 +0800

    webpack模板创建完成

---
+++ webpack_example/src/components/Hello.js
@@ -0,0 +1,18 @@
+
+import './Hello.css'
+import './Hello.scss'
+
+import React, {Component} from 'react'
+
+export default class Hello extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>只有你懂我风中的沉默</h1>
+                <br/>
+                <img/>
+            </div>
+        )
+    }
+}
